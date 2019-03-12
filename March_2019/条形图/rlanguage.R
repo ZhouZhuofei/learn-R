31a9d2e949a0b188896f1435788e9e26931be12d
ZhouZhuoFei <48236134+ZhouZhuofei@users.noreply.github.com>
2019-03-12 15:19:27 +0800

    Update rlanguage.R

--- March_2019/条形图/rlanguage.R
+++ March_2019/条形图/rlanguage.R
@@ -17,7 +17,28 @@ ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar)) + geom_bar(position = 
 #result Rplot14.png
 
 ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar)) + geom_bar(position = "dodge", stat = "identity", colour = "black")
-ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar)) + geom_bar(position = "dodge", stat = "identity", colour = "black")+scale_fill_brewer(palette = "Pastell")
+ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar)) + geom_bar(position = "dodge", stat = "identity", colour = "black") + scale_fill_brewer(palette = "Pastell")
 #
 #绘制频数条形图
+ggplot(diamonds, aes(x=cut)) + geom_bar()
+#result Rplot15.png
+ggplot(diamonds, aes(x=carat)) + geom_bar()
+#result Rplot16.png
 
+upc = subset(uspopchange, rank(Change)>40)
+ggplot(upc, aes(x=Abb, y=Change, fill=Region)) + geom_bar(stat = "identity")
+#result Rplot17.png
+
+ggplot(upc, aes(x=Abb, y=Change, fill=Region)) + geom_bar(stat = "identity", colour = "black") + scale_fill_manual(values = c("#669933", "#FFCC66")) + xlab("State")
+#result Rplot18.png
+
+#对正负条形图分别着色
+
+csub = subset(climate, Source=="Berkeley" & Year >= 1900)
+csub$pos = csub$Anomaly10y >=0
+ggplot(csub, aes(x=Year, y=Anomaly10y, fill=pos)) + geom_bar(stat = "identity", position = "identity")
+#result Rplot19.png
+#
+#change color
+ggplot(csub, aes(x=Year, y=Anomaly10y, fill=pos)) + geom_bar(stat = "identity", position = "identity", colour = "black", size =0.25) + scale_fill_manual(values=c("#CCEEFF", "#FFDDDD"), guide=FALSE)
+#reult Rplot20
